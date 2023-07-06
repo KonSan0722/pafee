2a1fb9845a46f92ebea9ae432aafd12d0bf03fc6
koki <oksg0722@gmail.com>
2023-07-06 09:45:33 +0900

    Textの変更

--- lib/presentation/widgets/fee_page_container.dart
+++ lib/presentation/widgets/fee_page_container.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pafee_app/Application/state/pafee_provider.dart';
+import 'package:pafee_app/presentation/widgets/calculate_text.dart';
+import 'package:pafee_app/presentation/widgets/preparation_text.dart';
 
 class LoginContainer extends ConsumerWidget {
   const LoginContainer({super.key});
@@ -10,10 +12,12 @@ class LoginContainer extends ConsumerWidget {
     final changeContainerNotifier = ref.watch(changeContainerNotifierProvider);
     final size = MediaQuery.of(context).size;
     final height = size.height;
+    final width = size.width;
     return Padding(
         padding: const EdgeInsets.only(left: 30, right: 30),
         child: AnimatedContainer(
-            height: changeContainerNotifier ? height / 4 * 2 : height / 6 * 4,
+            width: width / 10 * 8,
+            height: changeContainerNotifier ? height / 3 : height / 5,
             decoration: BoxDecoration(
                 border: Border.all(color: Colors.black),
                 borderRadius: BorderRadius.circular(20),
@@ -29,7 +33,7 @@ class LoginContainer extends ConsumerWidget {
             curve: Curves.bounceOut,
             duration: const Duration(milliseconds: 1000),
             child: changeContainerNotifier
-                ? const Text("かかかかっかかかかっかか")
-                : Text("いいいいいいいいい")));
+                ? const PreparationText()
+                : const CalculateText()));
   }
 }
