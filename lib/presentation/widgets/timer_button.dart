c40d58e3a227ee36bb2c216a9d07e2ed37d99296
koki <oksg0722@gmail.com>
2023-07-14 10:44:54 +0900

    タイマーの機能を追加

--- lib/presentation/widgets/timer_button.dart
+++ lib/presentation/widgets/timer_button.dart
@@ -1,15 +1,19 @@
 import 'dart:async';
+import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:go_router/go_router.dart';
 import 'package:pafee_app/Application/state/pafee_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pafee_app/presentation/widgets/end_alert.dart';
+
+import 'alert.dart';
 
 class TimerButton extends HookConsumerWidget {
-  const TimerButton({super.key});
-
+  bool isVisible;
+  TimerButton({super.key, required this.isVisible});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    var stopTimer = useState(false);
     var elapsedTime = useState(0);
     var newValue = useState(0);
     final timeNotifier = ref.watch(timeNotifierProvider);
@@ -18,10 +22,12 @@ class TimerButton extends HookConsumerWidget {
     final maximumTimeNotifier = ref.watch(maximumTimeNotifierProvider);
     final updateFeeNotifier = ref.watch(updateFeeNotifierProvider);
     final changeContainerNotifier = ref.watch(changeContainerNotifierProvider);
+    final stop = ref.watch(stopTimerNotifierProvider);
 
     void startElapsedTimer() {
       Timer.periodic(const Duration(seconds: 1), (timer) {
-        if (stopTimer.value == true) {
+        final stopNotifier = ref.read(stopTimerNotifierProvider);
+        if (stopNotifier == true) {
           if (elapsedTime.value == int.parse(maximumTimeNotifier)) {
             final notifier = ref.read(updateFeeNotifierProvider.notifier);
             notifier.save(int.parse(maximumFeeNotifier));
@@ -37,7 +43,8 @@ class TimerButton extends HookConsumerWidget {
 
     void startCalculateTimer() {
       Timer.periodic(Duration(seconds: int.parse(timeNotifier)), (timer) {
-        if (stopTimer.value == true) {
+        final stopNotifier = ref.read(stopTimerNotifierProvider);
+        if (stopNotifier == true) {
           if (elapsedTime.value == int.parse(maximumTimeNotifier) &&
               int.parse(maximumTimeNotifier) != 0) {
             timer.cancel();
@@ -52,15 +59,18 @@ class TimerButton extends HookConsumerWidget {
       });
     }
 
-    return ElevatedButton(
-        onPressed: () {
-          stopTimer.value = !stopTimer.value;
-          final notifier = ref.read(changeContainerNotifierProvider.notifier);
-          notifier.change();
-          startElapsedTimer();
-          startCalculateTimer();
-        },
-        child:
-            changeContainerNotifier ? const Text('計算開始') : const Text("計算終了"));
+    return Visibility(
+      visible: isVisible,
+      child: ElevatedButton(
+          onPressed: () {
+            final stopNotifier = ref.read(stopTimerNotifierProvider.notifier);
+            stopNotifier.change();
+            final notifier = ref.read(changeContainerNotifierProvider.notifier);
+            notifier.change();
+            startElapsedTimer();
+            startCalculateTimer();
+          },
+          child: const Text('計算開始')),
+    );
   }
 }
