1ea9b3cfdcf8edfe295248db5f9c9c1aae5de945
koki <oksg0722@gmail.com>
2023-07-14 10:42:52 +0900

    いらないコードを消し、visibleを追加

--- lib/presentation/pages/fee_page.dart
+++ lib/presentation/pages/fee_page.dart
@@ -1,9 +1,11 @@
 import 'dart:async';
 
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:pafee_app/presentation/widgets/back_home_Button.dart';
+import 'package:pafee_app/presentation/widgets/result_button.dart';
 import 'package:pafee_app/presentation/widgets/timer_button.dart';
 import '../../Application/state/pafee_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -12,30 +14,29 @@ import '../widgets/fee_page_container.dart';
 
 class FeePage extends HookConsumerWidget {
   FeePage({super.key});
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final timeNotifier = ref.watch(timeNotifierProvider);
-    final feeNotifier = ref.watch(feeNotifierProvider);
-    final maximumFeeNotifier = ref.watch(maximumFeeNotifierProvider);
-    final maximumTimeNotifier = ref.watch(maximumTimeNotifierProvider);
-    // int isTime = int.parse(timeNotifier) * 60;
     final changeContainerNotifier = ref.watch(changeContainerNotifierProvider);
     final updateFeeNotifier = ref.watch(updateFeeNotifierProvider);
     final changeTextContainerNotifer =
         ref.watch(changeTextContainerNotifierProvider);
+
     ref.listen(updateFeeNotifierProvider, (oldState, newState) {
       ScaffoldMessenger.of(context)
           .showSnackBar(SnackBar(content: Text("駐車料金が$newState円になりました")));
     });
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 255, 208, 52),
+      backgroundColor: const Color.fromARGB(255, 255, 208, 52),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             const LoginContainer(),
-            const TimerButton(),
+            TimerButton(isVisible: ref.read(changeContainerNotifierProvider)),
+            changeContainerNotifier ? Container() : const ResultButton(),
             changeContainerNotifier ? const BackHomeButton() : Container(),
           ],
         ),
